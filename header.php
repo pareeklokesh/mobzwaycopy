79e3781ac5068a8fb5577a2c1a24b9ffb6d1d81a
pareeklokesh <104905276+pareeklokesh@users.noreply.github.com>
2023-05-17 23:31:50 +0530

    Add files via upload

--- header.php
+++ header.php
@@ -1,94 +1,245 @@
 <!DOCTYPE html>
 <html>
 <head>
-  <meta charset="utf-8">
-  <meta name="viewport" content="width=device-width, initial-scale=1">
-  <title>Mobzway Copy</title>
-<link href="https://fonts.googleapis.com/css2?family=Open+Sans:wght@400;500;600;700;800&display=swap" rel="stylesheet">
-<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/font-awesome/4.5.0/css/font-awesome.min.css">  
+        <meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <meta http-equiv="Cache-Control" content="no-cache, no-store, must-revalidate">
+        <?php $baseurl =  "https://" . $_SERVER['SERVER_NAME'] .'/'; ?>
+        <?php include 'meta.php';?>
+        <link rel="icon" href="<?=  $baseurl ?>assets/images/favicon1.png" sizes="32x32" />
+        <link rel="stylesheet" href="<?=  $baseurl ?>assets/css/bootstrap.min.css">
+        <link rel="stylesheet" href="<?=  $baseurl ?>assets/css/style.css">
+        <link rel="stylesheet" type="text/css" href="<?=  $baseurl ?>assets/css/styleheader.css">
+        <link rel="stylesheet" media="print" onload="this.onload=null;this.removeAttribute('media');" href="<?=  $baseurl ?>assets/css/all.min.css">
+        <link rel="stylesheet" media="print" onload="this.onload=null;this.removeAttribute('media');" href="<?=  $baseurl ?>assets/css/animate.css">
+        <link rel="stylesheet" type="text/css" media="print" onload="this.onload=null;this.removeAttribute('media');" href="<?=  $baseurl ?>assets/css/style_ra.css">
+        <link rel="stylesheet" media="print" onload="this.onload=null;this.removeAttribute('media');" type="text/css" href="<?=  $baseurl ?>assets/css/aos.css">
+        <link rel="stylesheet" media="print" onload="this.onload=null;this.removeAttribute('media');" type="text/css" href="<?=  $baseurl ?>assets/css/owl.css">
+        <link rel="stylesheet" media="print" onload="this.onload=null;this.removeAttribute('media');" type="text/css" href="<?=  $baseurl ?>assets/css/owl.carousel.min.css">
+        <link rel="stylesheet" media="print" onload="this.onload=null;this.removeAttribute('media');" type="text/css" href="<?=  $baseurl ?>assets/css/owl.theme.default.min.css">
+        
+    <link rel="stylesheet" media="print" onload="this.onload=null;this.removeAttribute('media');" type="text/css" href="<?=  $baseurl ?>assets/css/slick.min.css">
+    <link rel="stylesheet" media="print" onload="this.onload=null;this.removeAttribute('media');" type="text/css" href="<?=  $baseurl ?>assets/css/slick-theme.min.css">
+    <meta name="google-site-verification" content="iNOnO6qiU2-w90b4anRR28cExx9pzKzxyxUhC-DD8vw" />
 
-<link rel="icon" type="text/css" href="assets/img/favicon.png"> 
-<link rel="stylesheet" href="assets/css/bootstrap.min.css">
-<link rel="stylesheet" href="assets/css/style.css">
-<link rel="stylesheet" href="assets/css/owl.carousel.min.css">
-<link rel="stylesheet" type="text/css" href="assets/css/aos.css">
- <link href="https://fonts.googleapis.com/css2?family=Montserrat:wght@100;200;300;400;500;600;700;800;900&display=swap" rel="stylesheet">  
-  <link href="https://fonts.googleapis.com/css2?family=Josefin+Sans:ital,wght@0,400;0,500;0,600;0,700;1,700&display=swap" rel="stylesheet">  
+<script>
 
-<script src="assets/js/bootstrap.min.js"></script>
-<script src="assets/js/jquery.min.js"></script>
-<script src="assets/js/owl.carousel.js"></script>
-<script src="assets/js/aos.js"></script>
-</head>
+if(navigator.userAgent.indexOf("Chrome-Lighthouse") == -1) { 
+
+// Google Tag Manager as Payload for all 3rd-party JS
+(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+'https://www.googletagmanager.com/gtag/js?id=UA-70164754-1'+i+dl;f.parentNode.insertBefore(j,f);
+})(window,document,'script','dataLayer','GTM-XXXXXXXXXX');
+
+ }
+</script>
+<script type='application/ld+json'>
+{
+  "@context": "http://www.schema.org",
+  "@type": "Organization",
+  "name": "Mobzway Technologies",
+   "telephone": "+91 7878044044",
+  "url": "https://www.mobzway.com/",
+  "logo": "https://www.mobzway.com/assets/images/logo.png",
+  "description": "Mobzway is one of the top poker and casino game development company. We Develop custom poker, casino, rummy, ludo and teen patti game.",
+  "address": {
+    "@type": "PostalAddress",
+    "streetAddress": "G-4, Okay Plus Spaces, Malviya Nagar Industrialrea",
+    "addressLocality": "Jaipur",
+    "addressRegion": "Rajasthan",
+    "postalCode": "302017",
+    "addressCountry": "India"
+  },
+  "contactPoint": {
+    "@type": "PostalAddress",
+    "contactType": "Customer Service",
+    "telephone": "+91 7878044044"
+ },
+ "aggregateRating" : {
+    "@type" : "AggregateRating",
+    "ratingValue" : "5",
+    "bestRating" : "5",
+    "worstRating" : "1",
+    "ratingCount" : "127"
+  },
+  "review" : {
+    "@type" : "Review",
+    "author" : {
+      "@type" : "Person",
+      "name" : "Sagar Singh"
+    },
+    "datePublished" : "2021-11-09",
+    "reviewRating" : {
+      "@type" : "Rating",
+      "ratingValue" : "5",
+      "bestRating" : "5",
+      "worstRating" : "1"
+    },
+    "reviewBody": " This company makes games that work on many technologies and is the best company to work for and its working environment is very good."
+  }
+}
+ 
+}
+</script>
+
+
+    
+    </head>
 <body>
-<div class="full_page">
-<header class="container-fluid header">
-  <div class="header_inner">
-  <div class="row">
-    <div class="col-md-12 mb-center">
-      <a href="tel:7878 044 044" class="mb_desk align-items-center call_btn"><i class="fa fa-phone-alt"></i> 7878 044 044 <strong>Call Us Now!</strong></a>
-      <ul class="header_top_contact">
-        <li>
-          <a href="tel:+91-9116005595"><i class="fa fa-whatsapp"></i> +91-9116005595</a>
-        </li>
-        <li>
-          <a href="tel:+91-7878-044-044"><i class="fa fa-phone-alt"></i>+91-7878-044-044</a>
-        </li>
-        <li>
-          <a href="mailto:sales@mobzway.com"><i class="fa fa-envelope"></i>sales@mobzway.com</a>
-        </li>
-      </ul>
-    </div>
-  </div>
-  <div class="header__wrapper row justify-content-between align-items-center">
-    <div class="w-auto header_logo">
-      <a href="index.html">
-      <img src="assets/img/logo.png" />
-    </a>
-    </div>
-    <button class="mobile_menu">
-      <span></span>
-      <span></span>
-      <span class="mb-0"></span>
-    </button>
-    <div class="header_nav w-auto">
-      <ul class="navbar-nav header_nav_menu d-flex">
-        <li class="nav-item dropdown_menu">
-          <a class="nav-link active dropdown-toggle" id="navbarDropdown" role="button">Solutions</a>
-          <ul class="sub-menu" aria-labelledby="navbarDropdown">
-            <li><a class="dropdown-item" href="#">Turnkey</a></li>
-            <li><a class="dropdown-item" href="#">Usports API</a></li>
-            <li><a class="dropdown-item" href="#">Betshop Solutions</a></li>
+<!-- Header-->
+<div class="fullpage">
+              <header id="header">
+                  <div class="container" style="max-width: 1250px;">
+                      <div class="top_header d-flex justify-content-between">
+                        <div class="top_contact w-100">
+                            <div class="top_left">
+                                <ul class="d-flex justify-content-end">
+                                  <li class="t_wrapper">
+                                      <div class="t_wrapper d-flex">
+                                        <a href="https://api.whatsapp.com/send?phone=919116005595"><span class="pr-1"><i class="fab fa-whatsapp"></i></span>+91-9116005595</a>
+                                      </div>
+                                  </li>
+                                  <li>
+                                      <div class="t_wrapper d-flex">
+                                        <a href="tel:+91-7878-044-044"><span class="pr-1"><i class="fas fa-phone-alt"></i></span>+91-7878-044-044</a>
+                                      </div>
+                                  </li>
+                                   <li class="responsive_call_sec">
+                                      <div class="t_wrapper d-flex call_number">
+                                        <a href="tel:+91-7878-044-044"><span class="pr-1"><i class="fas fa-phone-alt"></i></span>7878 044 044</a>
+                                      </div>
+                                      <div class="t_wrapper d-flex">
+                                        <a href="tel:+91-7878-044-044"><span class="pr-1"></span>CALL US NOW !</a>
+                                      </div>
+                                  </li>
+                                    <li>
+                                        <div class="t_wrapper d-flex">
+                                            <a href="mailto:sales@mobzway.com">
+                                              <span class="pr-1"><i class="fas fa-envelope"></i></span>sales@mobzway.com</a>
+                                        </div>
+                                    </li>
+                                </ul>
+                            </div>
+                        </div>
+                      </div>
+                      <div id="menus_lnk" class="d-flex justify-content-between">
+                          <div class="brand_wrapper align-self-center">
+                              <a href="<?=  $baseurl ?>" class="brand">
+                                <img src="<?=  $baseurl ?>assets/images/logo.png" width="1600" height="900" alt="Mobzway Logo"></a>
+                          </div>
+                          <button class="responsive-nav" onclick="openNav()">
+                              <span class="line trun"></span>
+                              <span class="line ops"></span>
+                              <span class="line trback"></span>
+                          </button>
+                          <div class="left_nav mt-3">
+                            
+                          <ul id="menu-header-menu" class="d-flex">
+                          	<li id="menu-item-7178" class="menu-item menu-item-type-custom menu-item-object-custom menu-item-7178">
+                          		<a href="<?=  $baseurl ?>poker-software/">Poker</a>
+                          	</li>
+      <li id="" class="menu-item">
+      	<a href="<?=  $baseurl ?>rummy-software/">Rummy</a>
+      </li>
+      <li id="" class="menu-item">
+      	<a href="<?=  $baseurl ?>ludo-game-development/">Ludo</a>
+      </li>
+      <li id="" class="menu-item">
+      	<a href="<?=  $baseurl ?>teen-patti-software/">Teen Patti</a>
+      </li>
+      <li id="" class="menu-item">
+      	<a href="<?=  $baseurl ?>casino-software/">Casino</a>
+      </li>
+      <li class="has_child menu-item  menu-item-74">
+      	<a href="#">More Solutions</a>
+      <ul class="sub-menu">
+          <li id="menu-item-75" class="menu-item">
+          	<a href="<?=  $baseurl ?>poker-game-development/">Poker Game Development</a>
+          <ul class="sub-menu">
+              <li id="menu-item-76" class="menu-item">
+              	<a href="<?=  $baseurl ?>texas-holdem-poker/">Texas Holdem Poker</a>
+              </li>
+              <li id="menu-item-77" class="menu-item">
+              	<a href="<?=  $baseurl ?>omaha-poker-game-development/">Omaha Poker Game Development</a>
+              </li>
+              <li id="menu-item-78" class="menu-item">
+              	<a href="<?=  $baseurl ?>ofc-poker-game-development/">OFC Poker Game Development</a>
+              </li>
+              <li id="menu-item-79" class="menu-item">
+              	<a href="<?=  $baseurl ?>seven-stud-poker/">Seven Stud Poker</a>
+              </li>
           </ul>
-        </li>
-        <li class="nav-item">
-          <a class="nav-link" href="#">Usports</a>
-        </li>
-        <li class="nav-item">
-          <a class="nav-link" href="#">
-            Ucasino
-          </a>
-        </li>
-        <li class="nav-item">
-          <a class="nav-link" href="#">Utools</a>
-        </li>
-        <li class="nav-item">
-          <a class="nav-link" href="#">News</a>
-        </li>
-        <li class="nav-item">
-          <a class="nav-link" href="#">About Us</a>
-        </li>
-        <li class="nav-item">
-          <a class="nav-link" href="#">Contact us</a>
-        </li>
-        <li class="nav-item request_btn">
-          <button data-bs-toggle="modal" data-bs-target="#requestQoute">Request a Quote</button>
-        </li>
-         
+      </li>
+          <li id="menu-item-7408" class="menu-item">
+          	<a href="#">Casino Game Development</a>
+          <ul class="sub-menu">
+              <li id="menu-item-83" class="menu-item">
+              	<a href="<?=  $baseurl ?>blackjack-game-development/">Blackjack Game Development</a>
+              </li>
+              <li id="menu-item-82" class="menu-item">
+              	<a href="<?=  $baseurl ?>baccarat-game-development/">Baccarat Game Development</a>
+              </li>
+              <li id="menu-item-85" class="menu-item">
+              	<a href="<?=  $baseurl ?>roulette-game-development/">Roulette Game Development</a>
+              </li>
+              <li id="menu-item-86" class="menu-item">
+              	<a href="<?=  $baseurl ?>slot-game-development/">Slot Game Development</a>
+              </li>
+              <li id="menu-item-81" class="menu-item">
+              	<a href="<?=  $baseurl ?>andar-bahar-game-development/">Andar Bahar Game Development</a></li>
+              <li id="menu-item-87" class="menu-item">
+              	<a href="<?=  $baseurl ?>triple-chance-game-development/">Triple Chance Game Development</a>
+              </li>
+              <li id="menu-item-90" class="menu-item">
+              	<a href="<?=  $baseurl ?>bingo-software/">Bingo Software</a>
+              </li>
+              <li id="menu-item-84" class="menu-item">
+              	<a href="<?=  $baseurl ?>keno-game-development/">Keno Game Development</a>
+              </li>
+          </ul>
+      </li>
+          <li id="menu-item-91" class="menu-item">
+          	<a href="<?=  $baseurl ?>rummy-game-development/">Rummy Game Development</a></li>
+          <li id="menu-item-92" class="menu-item">
+          	<a href="<?=  $baseurl ?>teen-patti-game-development/">Teen Patti Game Development</a></li>
+          <li id="menu-item-7182" class="menu-item">
+          	<a href="<?=  $baseurl ?>live-casino-software/">Live Casino Solution</a>
+          </li>
+          <li id="menu-item-93" class="menu-item">
+          	<a href="<?=  $baseurl ?>pool-game-development/">Pool Game Development</a>
+          </li>
+          <li id="menu-item-95" class="menu-item">
+          	<a href="<?=  $baseurl ?>card-game-development/">Card Game Development</a>
+          </li>
+          <li id="menu-item-96" class="menu-item  menu-item-96">
+          	<a href="#">White Label Solution</a>
+          <ul class="sub-menu">
+              <li id="menu-item-98" class="menu-item">
+              	<a href="<?=  $baseurl ?>white-label-casino-software/">White Label Casino Software</a>
+              </li>
+              <li id="menu-item-99" class="menu-item">
+              	<a href="<?=  $baseurl ?>white-label-poker-software/">White Label Poker Software</a>
+              </li>
+              <li id="menu-item-97" class="menu-item">
+              	<a href="<?=  $baseurl ?>igaming-software-provider/">iGaming Software Provider</a>
+              </li>
+          </ul>
+      </li>
       </ul>
-    </div>
-  </div>
-</div>
-</header>
+      </li>
+      <li id="menu-item-7296" class="contact_btn_list_wrapper menu-item menu-item-type-custom menu-item-object-custom menu-item-7296"><a href="#requestQoute">Request a Quote </a></li>
+      </ul>								
+      
+                          </div>
+                      </div>
+                  </div>
+                  
+              </header>
+            <!-----Header------->
 
-<!-- Header End! -->
+
+
+
+            
